4ca8b874cadc0adfbf190dfe2169ee3153844150
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-03-21 22:22:35 +0200

    Added Language class

---
+++ LibraryManager.DAL/Entities/Language.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.DAL.Entities
+{
+    class Language
+    {
+        /// <summary>
+        /// Language of the book
+        /// </summary>
+        public string LanguageName { get; set; }
+    }
+}
