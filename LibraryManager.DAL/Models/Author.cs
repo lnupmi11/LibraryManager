9be8692e54d956d2fbba3f02cbf507f438d7846a
Bohdan <bondarets.bogdan@gmail.com>
2019-03-21 20:56:52 +0200

    Creation of DAL and making simple Author entity

---
+++ LibraryManager.DAL/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.DAL.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int  NumberOfWrittenBooks { get; set; }
+    }
+}
