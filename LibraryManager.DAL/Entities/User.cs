f489d8239503a11c9a94a1ecb7f557cb2455f76e
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-03-21 21:47:03 +0200

    Added User class

---
+++ LibraryManager.DAL/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.DAL.Entities
+{
+    class User
+    {
+        /// <summary>
+        /// Id of the user in database
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Users' firstname
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Users' lasstname
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Whishlist of each users
+        /// </summary>
+        public IEnumerable<Book> Whishlist { get; set; }
+
+        /// <summary>
+        /// Collection of readed books
+        /// </summary>
+        public IEnumerable<Book> ReadedBooksCollection { get; set; }
+    }
+}
