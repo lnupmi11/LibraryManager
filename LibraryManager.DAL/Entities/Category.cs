cf9d09cfca164ee791f9264969e7f80860b1e34c
Nazar <nazar.hrytsiv7@gmail.com>
2019-03-21 23:33:23 +0300

    added class Category

---
+++ LibraryManager.DAL/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Models
+{
+    public class Category
+    {
+        /// <summary>
+        /// Category of the book
+        /// </summary>
+        public string CategoryName { get; set; }
+    }
+}
