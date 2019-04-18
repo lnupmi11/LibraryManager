77ecd5a506094fa4d96bdc678c11167355832e3b
Bohdan <bondarets.bogdan@gmail.com>
2019-04-18 22:51:19 +0300

    author repository

---
+++ LibraryManager.DAL/Reposetories/AuthorRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibraryManager.DAL.Entities;
+using LibraryManager.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManager.DAL.Reposetories
+{
+    public class AuthorRepository : IRepository<Author>
+    {
+        private readonly LibraryManagerContext _dbContext;
+
+        public BookRepository(LibraryManagerContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            return _dbContext.Authors;
+        }
+
+        public Author Get(int id)
+        {
+            return _dbContext.Authors.Find(id);
+        }
+
+        public void Create(Author item)
+        {
+            _dbContext.Authors.Add(item);
+        }
+
+        public void Update(Author item)
+        {
+            _dbContext.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            var author = _dbContext.Authors.Find(id);
+            if (author != null)
+                _dbContext.Authors.Remove(author);
+        }         
+    }
+}
