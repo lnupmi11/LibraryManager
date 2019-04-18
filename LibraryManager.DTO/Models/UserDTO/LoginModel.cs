16b9c59281c0048635bb756e4d65dfb15c251535
Nazar <nazar.hrytsiv7@gmail.com>
2019-04-19 01:08:48 +0300

    added DTO for User

---
+++ LibraryManager.DTO/Models/UserDTO/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManager.DTO.Models.UserDTO
+{
+    class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+    }
+}
