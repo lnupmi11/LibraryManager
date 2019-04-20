53723cb35ab4a3ab96e3ad0d7252716fa55f5c57
Bohdan <bondarets.bogdan@gmail.com>
2019-04-20 12:29:58 +0300

    created author dto

---
+++ LibraryManager.DTO/Models/AuthorDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.DTO.Models
+{
+    class AuthorDTO
+    {
+        public string FullName { get; set; }
+        public int NumberOfWrittenBooks { get; set; }
+        public ICollection<BookDTO> Books { get; set; }
+    }
+}
+
