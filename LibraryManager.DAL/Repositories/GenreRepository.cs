0c03caad346f04dd7bbe2d2c3fcba5e5c2b2d947
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-04-24 01:03:21 +0300

    add service,dto.. for genre

---
+++ LibraryManager.DAL/Repositories/GenreRepository.cs
@@ -0,0 +1,54 @@
+﻿using LibraryManager.DAL.Entities;
+using LibraryManager.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryManager.DAL.Repositories
+{
+    class GenreRepository:IRepository<Genre,int>
+    {
+        private readonly LibraryManagerContext _dbContext;
+
+        public GenreRepository(LibraryManagerContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<Genre> GetAll()
+        {
+            return _dbContext.Genres;
+        }
+
+        public Genre Get(int id)
+        {
+            return _dbContext.Genres.Find(id);
+        }
+
+        public Genre GetByName(string genreName)
+        {
+            return _dbContext.Genres.FirstOrDefault(x => x.GenreName == genreName);
+        }
+
+        public void Create(Genre item)
+        {
+            _dbContext.Add(item);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Genre item)
+        {
+            _dbContext.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            var genre = _dbContext.Genres.Find(id);
+            if (genre != null)
+                _dbContext.Genres.Remove(genre);
+            _dbContext.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ LibraryManager.DTO/Models/GenreDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.DTO.Models
+{
+    public class GenreDTO
+    {
+        public int Id { get; set; }
+
+        public string GenreName { get; set; }
+    }
+}
