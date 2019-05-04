e059b801aa1b2603b8dd9840966e3333b20c3e17
Bohdan <bondarets.bogdan@gmail.com>
2019-05-04 23:03:55 +0300

    fixing bug for previous commit.Now you can add book to your wishlist

--- LibraryManager.DAL/Entities/UserBook.cs
+++ LibraryManager.DAL/Entities/UserBook.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace LibraryManager.DAL.Entities
 {
     public class UserBook
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string UserId { get; set; }
         
         public User User { get; set; }
