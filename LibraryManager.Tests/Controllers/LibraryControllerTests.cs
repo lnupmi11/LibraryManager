d590bcb98c34314b3a866fce418819d82505c55f
DmytroRomaniv <d.romanivvv@gmail.com>
2019-05-24 17:47:26 +0300

    Changed rate for users who read or reading this book. Created identity mockups

---
+++ LibraryManager.Tests/Controllers/LibraryControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using LibraryManager.BLL.Interfaces;
+using LibraryManager.BLL.Services;
+using Moq;
+using LibraryManager.DAL.Interfaces;
+using LibraryManager.DAL.Entities;
+using AutoMapper;
+using LibraryManager.DTO.Models;
+using LibraryManager.DTO.Models.Manage;
+using LibraryManager.DAL;
+using System.Linq;
+using LibraryManagerControllers;
+using Microsoft.AspNetCore.Identity;
+using LibraryManager.Tests.CustomMocks;
+
+namespace LibraryManager.Tests.Controllers
+{
+    public class LibraryControllerTests
+    {
+        private readonly LibraryController libraryController;
+        private readonly Mock<IUserService> mockUserService;
+        private readonly Mock<IBookService> mockBookService;
+        private readonly Mock<IGenreService> mockGenreService;
+        private readonly Mock<IAdminService> mockAdminService;
+        private readonly MockRoleManager mockRoleManager;
+        private readonly MockSignInManager mockSignInManager;
+        private readonly MockUserManager mockUserManager;
+
+        public LibraryControllerTests()
+        {
+            mockUserService = new Mock<IUserService>();
+            mockBookService = new Mock<IBookService>();
+            mockGenreService = new Mock<IGenreService>();
+            mockAdminService = new Mock<IAdminService>();
+            mockRoleManager = new MockRoleManager();
+            mockSignInManager = new MockSignInManager();
+            mock
+        }
+
+        [Fact]
+
+    }
+}
---
+++ LibraryManager.Tests/CustomMocks/MockSignInManager.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManager.DAL.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Tests.CustomMocks
+{
+    public class MockSignInManager : SignInManager<User>
+    {
+        public MockSignInManager()
+                : base(new Mock<MockUserManager>().Object,
+                     new Mock<IHttpContextAccessor>().Object,
+                     new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                     new Mock<IOptions<IdentityOptions>>().Object,
+                     new Mock<ILogger<SignInManager<User>>>().Object,
+                     new Mock<IAuthenticationSchemeProvider>().Object)
+        { }
+    }
+}
